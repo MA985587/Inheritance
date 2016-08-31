4602201aaa19ccebb3340ec493080b32de982445
cmagnus <cmagnus@gmail.com>
2016-08-31 15:46:45 -0400

    Initial commit

---
+++ Inheritance.java
@@ -0,0 +1,11 @@
+/* Inheritance.java
+ * written by csi/cen 213 students fall 2016
+ * 
+ * A class for learning about inheritance.
+ */
+public class Inheritance {
+  
+  public static void main (String [] args) {
+
+  }
+}
