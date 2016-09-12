45ddb54a345f0b7824b8fe06c3a2f273dfb87223
cmagnus <cmagnus@gmail.com>
2016-09-12 15:26:53 -0400

    Establishes properties for Character class

--- Character.java
+++ Character.java
@@ -1,2 +1,10 @@
 public class Character {
+  // Properties:
+  private int[] stats; // HP, strength, mana
+  // current location of Character: x,y coordinate
+  private int[] location; 
+  // location where Character starts: x,y coordinate
+  private int[] spawnLocation; 
+
+
 }
