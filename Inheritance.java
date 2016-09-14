f82bbc2c9df7056158d2a7babf24af3dbda1e4aa
cmagnus <cmagnus@gmail.com>
2016-09-14 15:35:52 -0400

    Adds tests for constructor of Character

--- Inheritance.java
+++ Inheritance.java
@@ -7,16 +7,29 @@ public class Inheritance {
   
   public static void main (String [] args) {
     int[] stats = {5,6,7};
-    int[] location = {50,50};
-    int[] spawnLocation = {50,50};
+    int[] location = {40,41};
+    int[] spawnLocation = {50,51};
     Character dude = new Character(stats, location, spawnLocation);
+    // tests that Constructor initializes stats properly
+    System.out.print("Stats: ");
+    for (int i = 0; i < dude.getStats().length; i++) {
+       System.out.print(dude.getStats()[i] + " "); 
+    }
+    System.out.println("");
     
-    System.out.println(dude.stats);
-    System.out.println(dude.location);
-    System.out.println(dude.spawnLocation());
+    // tests that Constructor initializes location properly
+    System.out.print("Location: ");
+    for (int i = 0; i < dude.getLocation().length; i++) {
+       System.out.print(dude.getLocation()[i] + " "); 
+    }
+    System.out.println("");
+
+    // tests that Constructor initializes spawn location properly
+    System.out.print("Spawn Location: ");
+    for (int i = 0; i < dude.getSpawnLocation().length; i++) {
+       System.out.print(dude.getSpawnLocation()[i] + " "); 
+    }
+    System.out.println("");
     
-    System.out.println(dude.getStats());
-    System.out.println(dude.getLocation());
-    System.out.println(dude.getSpawnLocation());
   }
 }
